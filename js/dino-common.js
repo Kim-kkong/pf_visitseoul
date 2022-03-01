c80f498c5899b11342fc71bb193fc2296f5c2f74
Kim-kkong <red.tail.91@gmail.com>
2022-03-01 13:49:24 +0900

    20220301

--- js/dino-common.js
+++ js/dino-common.js
@@ -1,1 +1,16 @@
+// DinoWorks Common JS
+$(document).ready(function() {
+  preventDefaultAnchor();
+});
 
+
+function preventDefaultAnchor() {
+  $(document).on('click', 'a[href="#"]', function(e) {
+    e.preventDefault();
+  });
+}
+
+
+
+
+  
